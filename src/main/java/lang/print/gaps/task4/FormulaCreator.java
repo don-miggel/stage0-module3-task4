1f6ade0ddcda5644757de163fb1dd7bad26bf6d3
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-18 17:02:20 +0200

    Modyle 3, task 4 modified

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,9 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 3;
-        double res;
-        res = ((9*a*a - 5*b+2+a-7)*((a + b-4*a*b)/2));
+        float a = 1f;
+        float b = 3f;
+        float res;
+        res = ((9 * a * a - 5 * b + 2 + a - 7) * ( (a + b - 4 * a * b) / 2) );
+        System.out.println(res);
     }
 }
